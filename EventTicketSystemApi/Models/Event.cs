3bcb5b54cb1c5499cf18a2576b6af1256d1c4023
Luis Fernando Cadima Campos <nando1296@gmail.com>
2025-03-08 09:19:47 -0400

    Added Event Model

---
+++ EventTicketSystemApi/Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace EventTicketSystemApi.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int AvaibleSeats { get; set; }
+    }
+}
